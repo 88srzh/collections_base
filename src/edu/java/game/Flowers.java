a4aa036f7867ee6af41a4f4dc2c529a96502ebe0
srzh <smpetrov18@yandex.ru>
2020-08-28 15:51:46 +0400

    add flowers

--- src/edu/java/game/Flowers.java
+++ src/edu/java/game/Flowers.java
@@ -1,11 +1,9 @@
 package edu.java.game;
 
-import java.util.Set;
-
 public enum Flowers
 {
     ROSE(100,5,30),
-    LILY(85,4,24),
+    LILY(85,1,24),
     TULIP(65,2,20),
     LOTUS(90,4,28),
     ORCHID(75,3,25);
@@ -16,10 +14,10 @@ public enum Flowers
 
 
     Flowers(int price, int freshness, int stem) {
-    this.price = price;
-    this.freshness = freshness;
-    this.stem = stem;
-}
+        this.price = price;
+        this.freshness = freshness;
+        this.stem = stem;
+    }
 
     public int getPrice() {
         return price;
--- src/edu/java/game/Main.java
+++ src/edu/java/game/Main.java
@@ -1,12 +1,17 @@
 package edu.java.game;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.EnumSet;
 import static edu.java.game.Flowers.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        EnumSet<Flowers> flowersALl = EnumSet.of(ROSE,LILY,TULIP,LOTUS,ORCHID);
+        EnumSet<Flowers> flowersAll = EnumSet.of(ROSE,LILY,TULIP,LOTUS,ORCHID);
+
+        flowersAll.forEach(int uno : flowersAll) {;
 
 
     }
